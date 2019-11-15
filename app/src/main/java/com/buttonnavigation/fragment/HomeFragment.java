7826e7de5d5bbc29479e28c88b35ca5263b0d463
prabin123bhusal <55730269+prabin123bhusal@users.noreply.github.com>
2019-11-15 11:18:15 +0545

    Initial commit

---
+++ app/src/main/java/com/buttonnavigation/fragment/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.buttonnavigation.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.buttonnavigation.R;
+import com.buttonnavigation.adapter.StudentAdapter;
+import com.buttonnavigation.models.LocalUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+    private RecyclerView recyclerView;
+    List<LocalUser> localUserList=new ArrayList<>();
+    LocalUser localUser=new LocalUser();
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_home,container,false);
+        recyclerView=view.findViewById(R.id.recycleview);
+        localUserList=localUser.getUserList();
+
+        StudentAdapter studentAdapter=new StudentAdapter(localUserList);
+        recyclerView.setAdapter(studentAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return view;
+    }
+}
